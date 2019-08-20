46d0ea05cd95801d831d1de0fa8dd3bf4faaa303
FlavioBS <bzsflavio@gmail.com>
2019-08-20 07:52:57 -0300

    execução do comando para transpilar os codigos do index.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,18 @@ const path = require("path");
 module.exports = {
   entry: path.resolve(__dirname, "src", "index.js"), //local onde a entrada de informaçoes a ser tranformadas virao
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, "public"), // local onde sera lançado as informações traduzidas
+    filename: "bundle.js" //arquivo onde será salvo
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // escapar ponto e $ inficar que arquivo deve terminar com isso
+        exclude: /node_modules/, //ja está tudo transpilado
+        use: {
+          loader: "babel-loader" //tranpilar codigo dessa aplicação
+        }
+      }
+    ]
   }
 };
