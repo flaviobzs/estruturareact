88fbe9d180742f8140b23cf132da4e933c938189
FlavioBS <bzsflavio@gmail.com>
2019-08-21 09:07:44 -0300

    adicionar e remover itens

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -10,18 +10,60 @@ class TechList extends Component {
   //   }
   // }  substituido pela instalação do plugin onde as variaveis podem ser inicializadas fora do construtor
 
+  //não pode mudar essa variavel diretamente (usar setState)
   state = {
-    techs: ["NodeJS", "ReactJS"]
+    newTech: "",
+    techs: ["NodeJS", "ReactJS", "React native"]
+  };
+
+  handleInputChange = e => {
+    //this.state.newTech = e.target.value;
+    this.setState({ newTech: e.target.value }); //realizar a atualização do estado apenas com esse metodo
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    //para mudar um array ou objeto, deve-se recriar-lo do zero
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
+    //spred operation para copiar
+    console.log(this.state.newTech);
+  };
+
+  handleDelete = tech => {
+    this.setState({ tech: this.state.techs.filter.filter(t => t !== tech) });
+    console.log(tech);
   };
 
   render() {
     console.log(this.state);
-
     return (
-      <ul>
-        <li>X</li>
-        <li>Y</li>
-      </ul>
+      // fragmento
+      //<>
+      <form onSubmit={this.handleSubmit}>
+        <h1>{this.state.newTech}</h1>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>
+              {tech}
+              {/* chamar a função dentro de outra função porque quando montado o componete, ja seria chamado automaticamente */}
+              <button onClick={() => this.handleDelete(tech)} type="button">
+                Remover
+              </button>
+            </li>
+          ))}
+          {/* <li>X</li> */}
+          {/* <li>Y</li>  */}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+      </form>
+      //</>
     );
   }
 }
